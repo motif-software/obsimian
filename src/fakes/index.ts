e8dfd3925e87f2f0d32bcb9ed3174cf1586e665d
Oliver Lade <piemaster21@gmail.com>
2021-07-12 12:07:45 +1000

    Reorganise build to produce better lib output

---
+++ src/fakes/index.ts
@@ -0,0 +1,14 @@
+import ObsimianApp from "./ObsimianApp";
+import { Obsimian, ObsimianData } from "./Obsimian";
+import ObsimianMetadataCache from "./ObsimianMetadataCache";
+import ObsimianPlugin from "./ObsimianPlugin";
+import ObsimianVault from "./ObsimianVault";
+
+export {
+  Obsimian,
+  ObsimianData,
+  ObsimianApp,
+  ObsimianMetadataCache,
+  ObsimianPlugin,
+  ObsimianVault,
+};
